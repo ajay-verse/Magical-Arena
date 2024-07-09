fae23d444308b4e6ebfd0cc9cc0aeedd2c85926a
SatyaAjayRayalavarapu <rsa914016@gmail.com>
2024-07-09 14:16:07 +0530

    feat: print functions to print details using ansii color codes

--- internal/utils/helpers/helpers.go
+++ internal/utils/helpers/helpers.go
@@ -1,1 +1,21 @@
 package helpers
+
+import (
+	// Go Internal Packages
+	"fmt"
+
+	// Local Packages
+	player "magicalarena/internal/player"
+	x "magicalarena/internal/utils/constants"
+)
+
+func PrintWelcomeMessage() {
+	fmt.Println(x.CYAN + "\n\t✨ Welcome to Magical Arena ✨\n" + x.RESET)
+}
+
+func PrintPlayerDetails(player *player.Player) {
+	fmt.Printf(x.CYAN + "🧙 %s Details 🧙\n" + x.RESET, player.Name())
+	fmt.Printf(x.GREEN+"🩸 Health: %d  "+x.RESET, player.Health())
+	fmt.Printf(x.RED+"💪 Strength: %d  "+x.RESET, player.Strength())
+	fmt.Printf(x.PURPLE+"🗡️  Attack: %d\n\n"+x.RESET, player.Attack())
+}
