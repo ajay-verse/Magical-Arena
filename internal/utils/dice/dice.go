3b55d963ddd8e8621b3bc6eaec0caa8e2cd37dfc
SatyaAjayRayalavarapu <rsa914016@gmail.com>
2024-07-09 11:52:31 +0530

    feat: Add dice rolling functions for various dice types

--- internal/utils/dice/dice.go
+++ internal/utils/dice/dice.go
@@ -1,1 +1,22 @@
 package dice
+
+import "math/rand"
+
+// Roll a die with a specified number of sides
+func Roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
+
+// Roll a 6-sided die
+func RollD6() int {
+	return Roll(6)
+}
+
+// Roll multiple dice of a specified type
+func RollMultiple(sides, count int) []int {
+	rolls := make([]int, count)
+	for i := 0; i < count; i++ {
+		rolls[i] = Roll(sides)
+	}
+	return rolls
+}
