f3df2c57c987d281bb0a7164ee8224078b0b72c0
SatyaAjayRayalavarapu <rsa914016@gmail.com>
2024-07-09 12:40:00 +0530

    feat: game simulation done with debug messages

--- cmd/magicalarena/main.go
+++ cmd/magicalarena/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	match "magicalarena/internal/match"
+	player "magicalarena/internal/player"
+)
+
+func main() {
+	println("Welcome to Magical Arena")
+
+	player1 := player.NewPlayer("Bhairava", 50, 5, 10)
+	player2 := player.NewPlayer("Supreme Yaskin", 100, 10, 5)
+
+	match1 := match.CreateNewMatch(player1, player2)
+	res := match1.Fight()
+	println(res, "Wins")
+}
