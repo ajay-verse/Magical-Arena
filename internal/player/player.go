e24c6032f259ae7dbd469a0bc4b5fd0d5859bc8e
SatyaAjayRayalavarapu <rsa914016@gmail.com>
2024-07-09 10:08:21 +0530

    feat: Add Player struct and NewPlayer function to create new Player instance

--- internal/player/player.go
+++ internal/player/player.go
@@ -1,1 +1,19 @@
 package player
+
+// Player represents a character in Magical Arena Game with attributes given.
+type Player struct {
+	name     string // Name of the player.
+	health   int    // Health points of the player.
+	strength int    // Strength of the player.
+	attack   int    // Attack power of the player.
+}
+
+// NewPlayer creates a new Player instance with the given name, health, strength, and attack.
+func NewPlayer(name string, health int, strength int, attack int) *Player {
+	return &Player{
+		name:     name,
+		health:   health,
+		strength: strength,
+		attack:   attack,
+	}
+}
