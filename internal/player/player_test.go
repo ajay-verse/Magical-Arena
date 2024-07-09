fb90da3c547e3a80c9c3235df7a830cf8cd22dc4
SatyaAjayRayalavarapu <rsa914016@gmail.com>
2024-07-09 17:52:16 +0530

    chore: add detailed comments to player_test.go

--- internal/player/player_test.go
+++ internal/player/player_test.go
@@ -2,23 +2,29 @@ package player
 
 import "testing"
 
+// TestNewPlayer tests the NewPlayer function.
+// It creates a new player with the given name, health, attack, and strength.
+// It checks if the player's attributes are set correctly and if the function returns the expected error for invalid inputs.
 func TestNewPlayer(t *testing.T) {
 	p, _ := NewPlayer("Ajay", 100, 10, 15)
 	if p.Name() != "Ajay" || p.Health() != 100 || p.Strength() != 10 || p.Attack() != 15 {
 		t.Error("NewPlayer returned incorrect values")
 	}
 
-    _, err := NewPlayer("", 100, 10, 15)
-    if err == nil || err.Error() != "name cannot be empty" {
+	_, err := NewPlayer("", 100, 10, 15)
+	if err == nil || err.Error() != "name cannot be empty" {
 		t.Error("NewPlayer did not return expected error for empty name")
 	}
 
-    _, err = NewPlayer("Ajay", -100, 10, 15)
-    if err == nil || err.Error() != "atrributes cannot be negative" {
+	_, err = NewPlayer("Ajay", -100, 10, 15)
+	if err == nil || err.Error() != "atrributes cannot be negative" {
 		t.Error("NewPlayer did not return expected error for empty name")
 	}
 }
 
+// TestSetName tests the SetName method of the Player struct.
+// It sets a new name for the player and checks if the name is set correctly and
+// if the function returns the expected error for an invalid input.
 func TestSetName(t *testing.T) {
 	p, _ := NewPlayer("Ajay", 100, 10, 15)
 	err := p.SetName("")
@@ -31,6 +37,9 @@ func TestSetName(t *testing.T) {
 	}
 }
 
+// TestSetHealth tests the SetHealth method of the Player struct.
+// It sets a new health for the player and checks if the health is set correctly
+// and if the function returns the expected error for an invalid input.
 func TestSetHealth(t *testing.T) {
 	p, _ := NewPlayer("Ajay", 100, 10, 15)
 	err := p.SetHealth(-50)
@@ -43,6 +52,9 @@ func TestSetHealth(t *testing.T) {
 	}
 }
 
+// TestSetAttack tests the SetAttack method of the Player struct.
+// It sets a new attack for the player and checks if the attack is set correctly
+// and if the function returns the expected error for an invalid input.
 func TestSetAttack(t *testing.T) {
 	p, _ := NewPlayer("Ajay", 100, 10, 15)
 	err := p.SetAttack(-20)
@@ -55,6 +67,9 @@ func TestSetAttack(t *testing.T) {
 	}
 }
 
+// TestSetStrength tests the SetStrength method of the Player struct.
+// It sets a new strength for the player and checks if the strength is set correctly and
+// if the function returns the expected error for an invalid input.
 func TestSetStrength(t *testing.T) {
 	p, _ := NewPlayer("Ajay", 100, 10, 15)
 	err := p.SetStrength(-10)
